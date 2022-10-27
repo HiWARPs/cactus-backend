5b37a07d9fd54f58cf817df69bacd38bd41474f0
katerynamelnykova <102432364+katerynamelnykova@users.noreply.github.com>
2022-10-27 05:55:55 +0300

    [CACTUS-29] Execute the test query from the node.js server by using curl (#5)
    * Added compose.yml
    
    * modified compose.yml
    
    * compose.yml #2
    
    * compose.yml3
    
    * compose.yml4
    
    * Added mongojs and created a function that executes a test query to mongodb
    
    * Deleted the cactusMongoDB directory and the old compose.yml file

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const express = require("express");
 const app = express();
+const mongojs = require('mongojs');
+const collections = ['Electrons'];
+const db = mongojs('test', collections);
 
 // A security risk: Enabling CORS for development. But do NOT enable it for production.
 const cors = require('cors');
@@ -16,6 +19,7 @@ app.get("/projects", getProjects);
 app.get("/form/12", getForm);
 app.post("/form/12/execute", executeForm);
 app.get("/data", getData);
+app.get("/test", getTestData)
 
 app.listen(3001)
 console.log("Node.js Express server is listening on port 3001...")
@@ -26,7 +30,7 @@ console.log("Node.js Express server is listening on port 3001...")
 //
 
 function welcome(req, res) {
-  res.json({ message: "☃️️ Welcome to Project Vova. Our mock backend is ready for you.️ ☃️" });
+  res.json({ message: "☃️️ Welcome to Project Cactus. Our mock backend is ready for you.️ ☃️" });
 }
 
 function getProjects(req, res) {
@@ -59,4 +63,30 @@ function getData(req, res) {
   let rawData = fs.readFileSync(filename);
   let jsonData = JSON.parse(rawData);
   res.json(jsonData);    
+}
+
+function getTestData(req, res) {
+  const spin = 1;
+  const min = 20;
+  const max = 120;
+  const inc = 20;
+  db.Electrons.find({
+    '$expr': {
+      '$and': [
+        {'$eq': ['$Spin', spin ]}, 
+        {'$gte':['$Angle', min]},
+        {'$lte':['$Angle', max]},
+        {'$eq': [
+          { '$mod': [{'$subtract': ['$Angle', min]}, inc]}, 0]
+        },
+      ]}
+    },
+    function(err, docs) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.json(docs)
+      }
+    }
+  )
 }
