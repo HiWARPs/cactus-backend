69dbb6321f4bf62e0b3233cf01bcc7eedbac64fa
Rex Posadas <rposadas@redwoodlogistics.com>
2023-03-01 21:43:13 +0800

    connect to db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // create an express app
 const express = require("express")
 const app = express()
+const mongojs = require('mongojs')
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -10,6 +11,33 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
+
+app.post("/projects", createProject)
+
+
+// define the first route
+app.get("/projects", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+
+
+// Connect to DB
+const db = mongojs(process.env.DATABASE_URL, ['electrons'])
+
+db.on('error', function (err) {
+  console.log('database error', err)
+})
+
+db.on('connect', function () {
+  console.log('database connected')
+})
+
+
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
     () => console.log("Server is running..."));
+
+function createProject(req, res) {
+  const j = 5;
+}
