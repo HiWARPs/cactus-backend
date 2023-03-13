4c2b3f545e2fd3e7676deb98635cabc5240ae284
Rex Posadas <rexposadas@gmail.com>
2023-03-13 21:52:07 +0800

    create variables for param ids so that the code is more readable

--- controllers/form.js
+++ controllers/form.js
@@ -2,15 +2,16 @@ const asyncHandler = require('express-async-handler')
 const Project = require("../models/projects");
 
 const getForm = asyncHandler(async (req, res) => {
-    formID = req.params.id
+    const projectId = req.params.pid
+    const formId = req.params.id
 
-    const project = await Project.findById(req.params.pid)
+    const project = await Project.findById(projectId)
     if (!project) {
         res.status(400)
         throw new Error("project not found")
     }
 
-    const form = await project.forms.id(req.params.id)
+    const form = await project.forms.id(formId)
 
     res.status(200).json(form)
 })
