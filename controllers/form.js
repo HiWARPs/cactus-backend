74590af32c765e9f0950fbd7be8a85c1ce746308
DennisT98 <113253981+DennisT98@users.noreply.github.com>
2023-04-15 07:15:07 +0200

    add endpoint for updating forms (#24)

--- controllers/form.js
+++ controllers/form.js
@@ -30,7 +30,35 @@ const createForm = asyncHandler(async (req, res) => {
   res.status(200).json(project)
 })
 
+const updateForm = asyncHandler(async (req, res) => {
+
+  const projectId = req.params.pid
+  const formId = req.params.id
+
+  const project = await Project.findById(projectId)
+  if (!project) {
+    res.status(400)
+    throw new Error("project not found")
+  }
+
+  const form = await project.forms.id(formId)
+  if (!form) {
+    res.status(400)
+    throw new Error("form not found")
+  }
+
+  form.name = req.body.name;
+  form.description = req.body.description;
+  form.references = req.body.references;
+  form.xColumns = req.body.xColumns;
+  form.yColumns = req.body.yColumns;
+  const updatedForm = await project.save();
+
+  res.status(200).json(updatedForm)
+})
+
 module.exports = {
     getForm,
     createForm,
+    updateForm,
 }
